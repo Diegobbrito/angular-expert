4eab4e673182f15efe1ff655caf611b78ed09ff4
DiegoBrito <dibbrito@gmail.com>
2021-02-05 13:14:28 -0300

    Add cadastro-component

--- src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { __assign } from "tslib";
+import { Usuario } from "./models/usuario";
 
 @Component({
-  selector: 'app-cadastro',
-  template: `
-    <p>
-      cadastro works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styles: [],
 })
 export class CadastroComponent implements OnInit {
+  cadastroForm: FormGroup;
+  usuario: Usuario;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.cadastroForm = this.fb.group({
+      nome: [""],
+      cpf: [""],
+      email: [""],
+      senha: [""],
+      senhaConfirmacao: [""],
+    });
   }
 
+  adicionarUsuario() {
+    this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+  }
 }
