4ef827c086d2135e58f9b4585e1e6a0dcf07b842
DiegoBrito <dibbrito@gmail.com>
2021-02-05 11:26:38 -0300

    Initial commit

---
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  template: `
+    <p>
+      cadastro works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
