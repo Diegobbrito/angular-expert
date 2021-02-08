235336d833114f8ec645c7f8bbe5082cce5f73ec
DiegoBrito <dibbrito@gmail.com>
2021-02-08 11:53:39 -0300

    Add app guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,55 +1,66 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { APP_BASE_HREF } from "@angular/common";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { registerLocaleData } from "@angular/common";
-import localePt from "@angular/common/locales/pt";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 registerLocaleData(localePt);
 
-import { AppComponent } from "./app.component";
-import { SobreComponent } from "./institucional/sobre/sobre.component";
-import { ContatoComponent } from "./institucional/contato/contato.component";
-import { AppRoutingModule, rootRouterConfig } from "./app.routes";
-import { DataBindingComponent } from "./demos/data-binding/data-binding.component";
-import { ProdutoService } from "./produtos/produtos.service";
-import { ListaProdutoComponent } from "./produtos/lista-produto/lista-produto.component";
-import { CadastroComponent } from "./demos/reactiveForms/cadastro/cadastro.component";
-import { NgBrazil } from "ng-brazil";
-import { TextMaskModule } from 'angular2-text-mask';
-import { CustomFormsModule } from "ng2-validation";
-import { NavegacaoModule } from "./navegacao/navegacao.module";
-import { EditarProdutoComponent } from './demos/editar-produto/editar-produto.component';
+import { NgBrazil } from 'ng-brazil';
+import { TextMask } from 'ng-brazil';
+import { CustomFormsModule } from 'ng2-validation'
+
+import { AppComponent } from './app.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { rootRouterConfig, AppRoutingModule } from './app.routes';
+import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
+import { NavegacaoModule } from './navegacao/navegacao.module';
+import { AuthGuard } from './services/app.guard';
+import { CadastroGuard } from './services/cadastro.guard';
+import { HttpClientModule } from '@angular/common/http';
+import { Provider } from '@angular/compiler/src/compiler_facade_interface';
+import { FilmesComponent } from './demos/pipes/filmes/filmes.component';
+import { FileSizePipe } from './demos/pipes/filmes/filesize.pipe';
+import { ImageFormaterPipe } from './demos/pipes/filmes/image.pipe';
+import { BarModule } from './demos/bar-di-zones/bar.module';
+import { BarServices } from './demos/bar-di-zones/bar.service';
+import { TodoModule } from './demos/todo-list/todo.module';
+
+export const BAR_PROVIDERS: Provider[] = [
+  BarServices
+  // pode adicionar outros e depois usar a coleção BAR_PROVIDERS;
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SobreComponent,
-    ContatoComponent,
-    DataBindingComponent,
-    ListaProdutoComponent,
     CadastroComponent,
-    EditarProdutoComponent,
+    FilmesComponent,
+    FilmesComponent,
+    FileSizePipe,
+    ImageFormaterPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule,
-    AppRoutingModule,
     ReactiveFormsModule,
+    NavegacaoModule,
+    HttpClientModule,
+    TextMask.TextMaskModule,
     NgBrazil,
     CustomFormsModule,
-    TextMaskModule,
-    NavegacaoModule
+    AppRoutingModule,
+    BarModule.forRoot({
+      unidadeId: 1000,
+      unidadeToken: 'eca938c99a0e9ff91029dc'
+    }),
+    TodoModule
   ],
   providers: [
-    ProdutoService,
-    {
-      provide: APP_BASE_HREF,
-      useValue: "/",
-    },
+    AuthGuard,
+    CadastroGuard,
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
