bb0bbf4dec4b478d24782e20262e95f9b8b9a42c
DiegoBrito <dibbrito@gmail.com>
2021-02-23 11:37:47 -0300

    Add home page

--- projeto-final/src/app/app.module.ts
+++ projeto-final/src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacaoModule } from './navegacao/navegacao.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NavegacaoModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
