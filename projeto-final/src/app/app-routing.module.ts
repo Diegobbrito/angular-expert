111a01a014ce2b82d3a16016a30445105c0bc4ab
DiegoBrito <dibbrito@gmail.com>
2021-02-23 13:41:58 -0300

    Add utils

--- projeto-final/src/app/app-routing.module.ts
+++ projeto-final/src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'conta',
-    // loadChildren: () => import('./conta/conta.module')
-      // .then(m => m.ContaModule)
+    loadChildren: () => import('./conta/conta.module')
+      .then(m => m.ContaModule)
   },
 
   { path: 'nao-encontrado', component: NotFoundComponent },
