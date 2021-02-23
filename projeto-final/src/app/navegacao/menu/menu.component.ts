0d9296832b030eae2016272a65bd3adf3daf9559
DiegoBrito <dibbrito@gmail.com>
2021-02-23 13:01:49 -0300

    Add backend yml

--- projeto-final/src/app/navegacao/menu/menu.component.ts
+++ projeto-final/src/app/navegacao/menu/menu.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
+  public isCollapsed: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.isCollapsed = true;
   }
 
 }
--- projeto-final/src/app/navegacao/navegacao.module.ts
+++ projeto-final/src/app/navegacao/navegacao.module.ts
@@ -6,6 +6,9 @@ import { MenuComponent } from './menu/menu.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { RouterModule } from '@angular/router';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 @NgModule({
   declarations: [
     FooterComponent,
@@ -13,7 +16,7 @@ import { RouterModule } from '@angular/router';
     MenuComponent,
     NotFoundComponent,
   ],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgbModule],
   exports: [FooterComponent, HomeComponent, MenuComponent, NotFoundComponent],
 })
 export class NavegacaoModule {}
