2aaee583b0dff08b0f2b2ebbf7193e6d30108f9f
DiegoBrito <dibbrito@gmail.com>
2021-02-24 13:40:24 -0300

    Update some components

--- projeto-final/src/app/navegacao/navegacao.module.ts
+++ projeto-final/src/app/navegacao/navegacao.module.ts
@@ -1,22 +1,37 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './menu/menu.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { RouterModule } from '@angular/router';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MenuLoginComponent } from './menu-login/menu-login.component';
+import { AcessoNegadoComponent } from './acesso-negado/acesso-negado.component';
 
 @NgModule({
-  declarations: [
-    FooterComponent,
-    HomeComponent,
-    MenuComponent,
-    NotFoundComponent,
-  ],
-  imports: [CommonModule, RouterModule, NgbModule],
-  exports: [FooterComponent, HomeComponent, MenuComponent, NotFoundComponent],
+    declarations: [
+        MenuComponent,
+        MenuLoginComponent,
+        HomeComponent,
+        FooterComponent,
+        NotFoundComponent,
+        AcessoNegadoComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        NgbModule
+    ],
+    exports: [
+        MenuComponent,
+        MenuLoginComponent,
+        HomeComponent,
+        FooterComponent,
+        NotFoundComponent,
+        AcessoNegadoComponent
+    ]
 })
-export class NavegacaoModule {}
+export class NavegacaoModule { }
